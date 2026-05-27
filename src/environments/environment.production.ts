ed923d7aca23f9d6331a027cc42f737a7a139f3f
Tavata <tavata.canales@gmail.com>
2026-05-27 17:49:53 -0500

    variables de entorno cambiadas a prod

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://backend-mayo-production.up.railway.app/api'
+  apiUrl: 'https://backend-modular-production.up.railway.app/api'
 };
